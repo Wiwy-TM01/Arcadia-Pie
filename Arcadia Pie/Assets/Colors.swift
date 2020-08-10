74049c6f9fefef3cbd855fdfb25b051b08cd8d57
Edward Barber <25310324+Wiwy-TM01@users.noreply.github.com>
2020-08-09 22:18:52 -0400

    - Create a navigation bar - Create a new .swift file to extend UIColors for our needs - Fill gap above navigation bar using UIBarPosition.topAttached

---
+++ Arcadia Pie/Assets/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  Arcadia Pie
+//
+//  Created by Edward A. Barber on 8/9/20.
+//  Copyright © 2020 Wiwy. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let easyBreeze = #colorLiteral(red: 0.1484992206, green: 0.5293092728, blue: 1, alpha: 1)
+    static let kindaGreen = #colorLiteral(red: 0.2460756004, green: 0.4534912705, blue: 0.1735473275, alpha: 1)
+}
